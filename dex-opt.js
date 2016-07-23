e2815a3cdf712345e048571a5e397875605bcb63
Murali Ramanujam <murali@dexecure.com>
2016-07-22 18:56:47 -0700

    setup for armadio, remove extra slash, handle css and js

--- dex-opt.js
+++ dex-opt.js
@@ -22,16 +22,11 @@ if (dexecure.optimisationsEnabled) {
     if (event.request.headers.has('Accept')) {
       headersToSendJS['Accept'] = event.request.headers.get('Accept');
     }
-    if (event.request.headers.has('Viewport-Width')) {
-      headersToSendJS['Dex-Viewport-Width'] = parseInt(event.request.headers.get('Viewport-Width'), 10) > 1300 ? 'l':'s';
-    }
-    if (event.request.headers.has('ETag')) {
-      headersToSendJS['ETag'] = event.request.headers.get('ETag');
-    }
+    
     var headersToSend = new Headers(headersToSendJS);
     var imageMatchRegex = new RegExp(dexecure.imageMatchRegex, "i");
     if (imageMatchRegex.test(event.request.url.toLowerCase()) && isFirstPartyDomain(event.request.url)) {
-     var dexecureURL = dexecure.server + event.request.url.replace(/^https?\:\/\//i, "");
+     var dexecureURL = dexecure.server + event.request.url.replace(/^https?\:\/\/(www.)?armadiofashion\.com\//i, "");
      dexecureURL = decodeURIComponent(dexecureURL);
      event.respondWith(fetch(dexecureURL, {mode: 'cors', headers: headersToSend})
       .then(response => {

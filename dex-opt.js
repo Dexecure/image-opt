f844ac2e9d6a05fcebcc3d7f6b2dffac75c2e657
Murali Ramanujam <murali@dexecure.com>
2016-07-24 16:59:16 -0700

    customisations for joy

--- dex-opt.js
+++ dex-opt.js
@@ -4,7 +4,7 @@ function urlContainsSignature(input) {
   return potentialSignature.includes('se') && potentialSignature.includes('st') && potentialSignature.includes('sp') && potentialSignature.includes('sv') && potentialSignature.includes('sr') && potentialSignature.includes('sig');
 }
 
-function separateSignatureAndURL(url) {
+function separateSignatureAndURL(input) {
   var potentialSignature = input.substring(input.lastIndexOf('/') + 1);
   var potentialURL = input.substring(0, input.lastIndexOf('/'));
   return {potentialURL, potentialSignature};
@@ -30,11 +30,18 @@ function convertToDexecureURL(url) {
 function isFirstPartyDomain(url) {
   var parsedURL =  new URL(url);
   var firstPartyDomain = dexecure.firstPartyDomain;
+  if (!firstPartyDomain) {
+    if (dexecure.debugMode) 
+      console.log('firstPartyDomain is null');
+    return false;
+  }
   for (var i = firstPartyDomain.length - 1; i >= 0; i--) {
     if (firstPartyDomain[i].toLowerCase() == parsedURL.host.toLowerCase()) {
       return true;
     }
   }
+  if (dexecure.debugMode)
+    console.log('firstPartyDomain is false for ', url);
   return false;
 }
 
@@ -59,35 +66,42 @@ if (dexecure.optimisationsEnabled) {
      var signatureComponent = '';
      dexecureURL = decodeURIComponent(dexecureURL);
      if (urlContainsSignature(dexecureURL)) {
+      if (dexecure.debugMode)
+        console.log('url contains signature');
       var separatedComponents = separateSignatureAndURL(dexecureURL);
-      dexecureURL = separatedComponents[0];
-      signatureComponent = separatedComponents[1];
+      if (dexecure.debugMode) 
+        console.log('separatedComponents is ', separatedComponents);
+      dexecureURL = separatedComponents['potentialURL'];
+      signatureComponent = separatedComponents['potentialSignature'];
      }
 
-     if (signatureComponent.length > 0) {
+     if (dexecure.debugMode)
+      console.log('Modified url is ', dexecureURL);
+
+     if (signatureComponent && signatureComponent.length > 0) {
       headersToSendJS['signaturepresent'] = 'true';
       headersToSendJS['signatureseparator'] = '/';
       headersToSendJS['signature'] = signatureComponent;
      }
 
      var headersToSend = new Headers(headersToSendJS);
-     event.respondWith(fetch(dexecureURL, {mode: 'cors', headers: headersToSend})
-      .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          if (dexecure.debugMode)
-            console.log('Responding with original image as optimiser was not reachable ', event.request.url);
-          throw new Error('Unable to fetch optimised image');
-        }
-      })
-      .catch(err => {
-        if (dexecure.debugMode) {
-          console.log('Sending original image as an error occured when trying to optimise ', event.request.url);
-          console.log('The error was ', err);
-        }
-        return fetch(event.request);
-      }));
+     event.respondWith(fetch(dexecureURL, {mode: 'no-cors', headers: headersToSend}));
+      // .then(response => {
+      //   if (response.ok) {
+      //     return response;
+      //   } else {
+      //     if (dexecure.debugMode)
+      //       console.log('Responding with original image as optimiser was not reachable ', event.request.url);
+      //     throw new Error('Unable to fetch optimised image');
+      //   }
+      // })
+      // .catch(err => {
+      //   if (dexecure.debugMode) {
+      //     console.log('Sending original image as an error occured when trying to optimise ', event.request.url);
+      //     console.log('The error was ', err);
+      //   }
+      //   return fetch(event.request);
+      // }));
    }
   })
 }

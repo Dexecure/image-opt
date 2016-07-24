4e7193494da817fffaf779e2b5db9a486955c0ec
Murali Ramanujam <murali@dexecure.com>
2016-07-24 15:37:47 -0700

    customise for joy

--- dex-opt.js
+++ dex-opt.js
@@ -1,3 +1,32 @@
+function urlContainsSignature(input) {
+  var potentialSignature = input.substring(input.lastIndexOf('/') + 1);
+  potentialSignature = potentialSignature.toLowerCase();
+  return potentialSignature.includes('se') && potentialSignature.includes('st') && potentialSignature.includes('sp') && potentialSignature.includes.('sv') && potentialSignature.includes('sr') && potentialSignature.includes('sig');
+}
+
+function separateSignatureAndURL(url) {
+  var potentialSignature = input.substring(input.lastIndexOf('/') + 1);
+  var potentialURL = input.substring(0, input.lastIndexOf('/'));
+  return {potentialURL, potentialSignature};
+}
+
+function convertToDexecureURL(url) {
+  var listOfJoyDomains = {'dev-media.withjoy.com':'dev-media-joy.dexecure.net',
+                          'rehearsal-media.withjoy.com': 'rehearsal-joy.dexecure.net',
+                          'dev.withjoy.com': 'dev-joy.dexecure.net',
+                          'rehearsal.withjoy.com': 'rehearsal-joy.dexecure.net',
+                          'ceremony-media.withjoy.com': 'ceremony-media-joy.dexecure.net',
+                          'withjoy.com': 'joy.dexecure.net'
+                          };
+  var parsedURL = new URL(url);
+  for (i in listOfJoyDomains) {
+    if (i.toLowerCase() == parsedURL.host.toLowerCase()) {
+      return url.replace(i, listOfJoyDomains[i]);
+    }
+  }
+  return url;
+}
+
 function isFirstPartyDomain(url) {
   var parsedURL =  new URL(url);
   var firstPartyDomain = dexecure.firstPartyDomain;
@@ -23,11 +52,25 @@ if (dexecure.optimisationsEnabled) {
       headersToSendJS['Accept'] = event.request.headers.get('Accept');
     }
     
-    var headersToSend = new Headers(headersToSendJS);
     var imageMatchRegex = new RegExp(dexecure.imageMatchRegex, "i");
     if (imageMatchRegex.test(event.request.url.toLowerCase()) && isFirstPartyDomain(event.request.url)) {
-     var dexecureURL = dexecure.server + event.request.url.replace(/^https?\:\/\//i, "");
+     var dexecureURL = convertToDexecureURL(event.request.url);
+     var signatureSeparator = '';
+     var signatureComponent = '';
      dexecureURL = decodeURIComponent(dexecureURL);
+     if (urlContainsSignature(dexecureURL)) {
+      var separatedComponents = separateSignatureAndURL(dexecureURL);
+      dexecureURL = separatedComponents[0];
+      signatureComponent = separatedComponents[1];
+     }
+
+     if (signatureComponent.length > 0) {
+      headersToSendJS['signaturepresent'] = 'true';
+      headersToSendJS['signatureseparator'] = '/';
+      headersToSendJS['signature'] = signatureComponent;
+     }
+
+     var headersToSend = new Headers(headersToSendJS);
      event.respondWith(fetch(dexecureURL, {mode: 'cors', headers: headersToSend})
       .then(response => {
         if (response.ok) {

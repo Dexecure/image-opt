ed0ffe0cf4d919606b2dafc053c8e21f3ea53743
Murali Ramanujam <murali@dexecure.com>
2016-07-24 20:45:25 -0700

    finish withjoy config

--- dex-opt.js
+++ dex-opt.js
@@ -58,8 +58,9 @@ if (dexecure.optimisationsEnabled) {
     if (event.request.headers.has('Accept')) {
       headersToSendJS['Accept'] = event.request.headers.get('Accept');
     }
-    
+    var headersToSend = new Headers(headersToSendJS);
     var imageMatchRegex = new RegExp(dexecure.imageMatchRegex, "i");
+
     if (imageMatchRegex.test(event.request.url.toLowerCase()) && isFirstPartyDomain(event.request.url)) {
      var dexecureURL = convertToDexecureURL(event.request.url);
      var signatureSeparator = '';
@@ -77,31 +78,29 @@ if (dexecure.optimisationsEnabled) {
 
      if (dexecure.debugMode)
       console.log('Modified url is ', dexecureURL);
-
+     let muraliHeaders = new Headers();
      if (signatureComponent && signatureComponent.length > 0) {
-      headersToSendJS['signaturepresent'] = 'true';
-      headersToSendJS['signatureseparator'] = '/';
-      headersToSendJS['signature'] = signatureComponent;
+      headersToSend.append('signaturepresent', 'true');
+      headersToSend.append('signatureseparator', '/');
+      headersToSend.append('signature', signatureComponent);
      }
-
-     var headersToSend = new Headers(headersToSendJS);
-     event.respondWith(fetch(dexecureURL, {mode: 'no-cors', headers: headersToSend}));
-      // .then(response => {
-      //   if (response.ok) {
-      //     return response;
-      //   } else {
-      //     if (dexecure.debugMode)
-      //       console.log('Responding with original image as optimiser was not reachable ', event.request.url);
-      //     throw new Error('Unable to fetch optimised image');
-      //   }
-      // })
-      // .catch(err => {
-      //   if (dexecure.debugMode) {
-      //     console.log('Sending original image as an error occured when trying to optimise ', event.request.url);
-      //     console.log('The error was ', err);
-      //   }
-      //   return fetch(event.request);
-      // }));
+     event.respondWith(fetch(dexecureURL, {mode: 'cors', headers: headersToSend})
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          if (dexecure.debugMode)
+            console.log('Responding with original image as optimiser was not reachable ', event.request.url);
+          throw new Error('Unable to fetch optimised image');
+        }
+      })
+      .catch(err => {
+        if (dexecure.debugMode) {
+          console.log('Sending original image as an error occured when trying to optimise ', event.request.url);
+          console.log('The error was ', err);
+        }
+        return fetch(event.request);
+      }));
    }
   })
 }
